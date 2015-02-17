d30639b6a32bc7d7fb5573ce22e6d7defb3a8938
Oleksandr Bystrikov <coder@yakviter.com.ua>
2015-02-17 02:40:36 +0200

    Change the way sinatra is reloading

--- app.rb
+++ app.rb
@@ -1,20 +1,35 @@
-require "sinatra"
+require "sinatra/base"
+require "sinatra/reloader"
 require "slim"
 require "slim/include"
 
-configure do
-  set :slim, :layout => :"layouts/default"
-  set :bind, "0.0.0.0"
-end
+module Softbeehive
+  class App < Sinatra::Base
+    
+    configure :development do
+      register Sinatra::Reloader
+    end
 
-get "/" do
-  slim :index
-end
+    configure do
+      set :slim, :layout => :"layouts/default"
+      set :bind, "0.0.0.0"
+    end
 
-get "/works" do 
-  slim :works
-end
+    get "/" do
+      slim :index
+    end
 
-get "/about" do 
-  slim :about
+    get "/works" do 
+      slim :works
+    end
+
+    get "/about" do 
+      slim :about
+    end
+
+    get "/contact" do 
+      slim :contact
+    end
+
+  end
 end
---
+++ config.rb
@@ -0,0 +1,14 @@
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "public/css"
+sass_dir = "public/sass"
+images_dir = "public/img"
+
+relative_assets = true
+
+# One of :expanded or :nested or :compact or :compressed
+output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+line_comments = false
