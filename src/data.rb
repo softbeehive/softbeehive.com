e0c0867972ae32e4ca32b17572c2960875b7d29d
Oleksandr Bystrikov <coder@yakviter.com.ua>
2015-03-02 00:17:50 +0200

    All project data

---
+++ src/data.rb
@@ -0,0 +1,105 @@
+module ProjectData
+  def data_array
+    @data = [
+      {
+        :name => "Identity.com",
+        :url => "www.identity.com",
+        :subtitle => "on behalf of Inflection",
+        :desc => "Identity.com is an identity management tool that helps people control their personal data and manage their credibility online.",
+        :slug => "identity",
+        :softstack => "Ruby on Rails, RSpec, JavaScript, Backbone, jQuery, Sass, Vagrant, Linux, Git",
+        :chronos => "May 2014 - present"
+      },
+      {
+        :name => "Adventum Sprachinstitut",
+        :url => "www.adventum.at",
+        :desc => "...",
+        :slug => "adventum",
+        :softstack => "Zend Framework 2, jQuery, Twitter Bootstrap 3, Sass, Graphic Design, Linux, Git",
+        :chronos => "April 2014"
+      },
+      {
+        :name => "Flickr Ocean Trawler",
+        :url => "ef.softbeehive.com",
+        :desc => "...",
+        :slug => "fot",
+        :softstack => "Zend Framework 2, Flickr API, MySQL, jQuery, Single Page Application, Linux, Graphic Design",
+        :chronos => "March 2014"
+      },
+      {
+        :name => "Buk Presentation Tool",
+        :url => "yakviter.com.ua/buk",
+        :desc => "...",
+        :slug => "buk",
+        :softstack => "JavaScript, jQuery, CSS3",
+        :chronos => "November 2013"
+      },
+      {
+        :name => "InEvention",
+        :url => "www.inevention.com",
+        :desc => "...",
+        :slug => "inevention",
+        :softstack => "PHP, Slim Framework, MongoDB, JavaScript, jQuery, Solr, CentOS, Subversion",
+        :chronos => "February 2013 - October 2013"
+      },
+      {
+        :name => "BuyerQuest",
+        :url => "/",
+        :desc => "...",
+        :slug => "buyerquest",
+        :softstack => "Magento, PHP",
+        :chronos => "November 2013 - February 2014"
+      },
+      {
+        :name => "EagleManager",
+        :desc => "...",
+        :slug => "eaglemanager",
+        :softstack => "Zend Framework 2, JavaScript",
+        :chronos => "September 2012 - January 2013"
+      },
+      {
+        :name => "Fairpros",
+        :desc => "...",
+        :slug => "fairpros",
+        :softstack => "Drupal 7",
+        :chronos => "November 2011 - May 2012"
+      },
+      {
+        :name => "Automore",
+        :desc => "...",
+        :slug => "",
+        :softstack => "Graphic Design",
+        :chronos => "June 2011"
+      },
+      {
+        :name => "Export Partner",
+        :desc => "...",
+        :slug => "",
+        :softstack => "",
+        :chronos => "July 2011"
+      },
+      {
+        :name => "Photoside",
+        :desc => "...",
+        :slug => "",
+        :softstack => "",
+        :chronos => "August 2011"
+      },
+      {
+        :name => "Nerz",
+        :desc => "...",
+        :slug => "",
+        :softstack => "",
+        :chronos => "September 2011"
+      },
+      {
+        :name => "A-GUS Akademie",
+        :desc => "...",
+        :slug => "",
+        :softstack => "",
+        :chronos => "May 2010 - June 2010"
+      }
+    ]
+    return @data
+  end
+end
