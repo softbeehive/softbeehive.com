631e559dab226443f715afc43becc73c2beb387a
Oleksandr Bystrikov <coder@yakviter.com.ua>
2015-03-02 00:16:40 +0200

    Updated routing, using helper

--- app.rb
+++ app.rb
@@ -8,6 +8,7 @@ module Softbeehive
     
     configure :development do
       register Sinatra::Reloader
+      also_reload "src/data.rb"
 
       set :slim, :layout => :"layouts/default"
       set :bind, "0.0.0.0"
@@ -16,16 +17,27 @@ module Softbeehive
     before { request.path_info.sub! %r{/$}, '' }
 
     get "/" do
-      slim :index
+      slim :projects
     end
 
-    get "/:section" do 
-      slim :"#{params[:section]}"
+    get "/projects" do
+      slim :projects
     end
 
-    get "/:section/:name" do
-      slim :"#{params[:section]}/#{params[:name]}"
+    get "/projects/:name" do
+      slim :"projects/#{params[:name]}"
     end
+
+    get "/about" do
+      slim :about
+    end
+
+    get "/contact" do
+      slim :contact
+    end
+
+    require_relative "src/data"
+    helpers ProjectData
 
   end
 end
