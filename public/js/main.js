8ac04d57340e69841ed82f2e8604ec433a9a9ab8
Oleksandr Bystrikov <coder@yakviter.com.ua>
2015-03-08 21:07:28 +0200

    Don't need srcset check stuff, because FF is going to support it soon

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,2 @@
 // Run svg check and replace with png if needed
 svgeezy.init(false, 'png');
-
-/*
-  Detect whether it is retina display or not and check combination 
-  of high density and srcset support
-*/
-var isRetina = window.devicePixelRatio >= 1.5,
-    listOfImages = document.getElementById("projects").getElementsByTagName("img"),
-    listLength = listOfImages.length,
-    isSrcset = listOfImages[0].srcset || false;
-
-if (isRetina && !isSrcset)  {
-  for (i = 0; i < listLength; ++i) {
-    listOfImages[i].src = listOfImages[i].src.split(".")[0] + "-2x.jpg";
-  }
-}
