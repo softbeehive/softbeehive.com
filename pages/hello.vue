d962a6a918d8f8420888b1569784c95a3610b021
Oleksandr Bystrikov <coder@yakviter.com.ua>
2020-08-11 17:26:42 +0200

    Generate nuxt app

---
+++ pages/hello.vue
@@ -0,0 +1,15 @@
+<template>
+  <article class="flex">
+    <nuxt-content :document="hello" />
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content }) {
+    const hello = await $content('hello').fetch()
+
+    return { hello }
+  }
+}
+</script>
