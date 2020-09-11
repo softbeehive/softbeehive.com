6419455b16759f91072c8d39ded3677aac86755c
Oleksandr Bystrikov <coder@yakviter.com.ua>
2020-09-11 15:26:51 +0200

    refactor(project): split project details

--- components/project/LazyProjectCompact.vue
+++ components/project/LazyProjectCompact.vue
@@ -1,7 +1,9 @@
 <template>
   <article class="project flex flex-wrap mb-8 md:mb-10 -mx-4">
     <div class="w-full lg:w-2/3 px-4">
-      <ProjectBrowser :project="project" />
+      <nuxt-link :to="`/projects/${project.slug}`" class="block">
+        <ProjectBrowser :project="project" />
+      </nuxt-link>
     </div>
     <div class="project-summary w-full lg:w-1/3 px-4">
       <ProjectSummary :project="project" />
@@ -21,3 +23,11 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" scoped>
+.project {
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+</style>
